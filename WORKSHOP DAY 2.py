37a649dcb79f2be58137edcacc19c1ae0fda97d7
Prashant Pandey <23prashantpandey@gmail.com>
2026-06-01 10:14:02 +0530

    Add files via upload

---
+++ WORKSHOP DAY 2.py
@@ -0,0 +1,23 @@
+#print('Prashant Pandey')
+#Program to find greatest number amoung a,b and c
+#a = int(input("Enter a Number1: "))
+#b = int(input("Enter a Number2: "))
+#c = int(input("Enter a Number3: "))
+#if(a>b):
+    #if(a>c):
+        #print("a is greater number")
+    #else:
+        #print("c is greater number")
+#else:
+    #print("b is greater number")
+#Program to find smallest number amoung a,b and c
+a = int(input("Enter a Number1: "))
+b = int(input("Enter a Number2: "))
+c = int(input("Enter a Number3: "))
+if(a>b):
+    if(a<c):
+        print("a is smallest number")
+    else:
+        print("c is smallest number")
+else:
+    print("b is smallest number")
