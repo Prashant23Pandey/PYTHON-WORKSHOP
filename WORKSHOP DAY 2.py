66dd0abaaefdc4f765cc6f84a722ac76145ba9c0
Prashant Pandey <23prashantpandey@gmail.com>
2026-06-01 15:52:24 +0530

    Update WORKSHOP DAY 2.py

--- WORKSHOP DAY 2.py
+++ WORKSHOP DAY 2.py
@@ -97,3 +97,89 @@ print("Sum of inputs =", total)
 n = int(input("Enter a number: "))
 for i in range(1, 11):
     print(n, "x", i, "=", n * i)
+# Program to compute factorial of a number
+n = int(input("Enter a number: "))
+fact = 1
+for i in range(1, n + 1):
+    fact = fact * i
+print("Factorial =", fact)
+#Program to check weather a number is Palindrome or not
+num = int(input("Enter a number"))
+N = num
+reverse = 0
+while(N!=0):
+    r = N%10
+    reverse = reverse*10+r
+    N = N//10
+print(f"Reverse of {num}: {reverse}")
+if(num == reverse):
+    print(f"{num} is a palindrome")
+else:343
+    print(f"{num} is not a palindrome")
+#Program to check weather a number is Armstrong or not
+num = int(input("Enter a number: "))
+temp = num
+power = len(str(num))
+total = 0
+while temp > 0:
+    digit = temp % 10
+    total += digit ** power
+    temp //= 10
+if total == num:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is not an Armstrong Number")
+#Program to Find the Sum of Even and Odd Numbers Separately
+n = int(input("Enter how many numbers: "))
+even_sum = 0
+odd_sum = 0
+for i in range(n):
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        even_sum += num
+    else:
+        odd_sum += num
+print("Sum of Even Numbers =", even_sum)
+print("Sum of Odd Numbers =", odd_sum)
+#Fibonacci Series Program
+n = int(input("Enter the number of terms: "))
+a = 0
+b = 1
+print("Fibonacci Series:")
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
+#Program to Find Factors of a Number
+num = int(input("Enter a number: "))
+print("Factors of", num, "are:")
+for i in range(1, num + 1):
+    if num % i == 0:
+        print(i)
+#Print a n×n Matrix with a symbol
+n = int(input("Enter numbers of terms"))
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+#Print a n×n Matrix with a number
+n = int(input("Enter numbers of terms"))
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(j, end=" ")
+    print()
+#Print a n×n Matrix within a Floyd's triangle
+n = int(input("Enter numbers of terms"))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+rows = int(input("Enter a Number"))
+number = 1
+print("Floyd's Triangle")
+for i in range(1, rows + 1):
+    for j in range(1 , i + 1):
+        print(number, end = "")
+        number = number + 1
+    print()
