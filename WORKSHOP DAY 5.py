b0e0ab5562a3718dfa154714ce31a230c8e1c1af
Prashant Pandey <23prashantpandey@gmail.com>
2026-06-04 11:54:52 +0530

    Update WORKSHOP DAY 5.py

--- WORKSHOP DAY 5.py
+++ WORKSHOP DAY 5.py
@@ -29,3 +29,28 @@ print(a(20))
 def show():
     print(a(8))
 show(lambda x:x)
+#IIFE(Immediately Involved Function Expression)
+(lambda x:print(x+1))(22)
+(lambda x,y:print(x+y))(17,6)
+def double(x):
+    return x*2
+li=[10,20,30,40,50]
+new_list=map(double,li)
+print(list(new_list))
+PRASHANT = [23, 24, 25, 26, 27]
+do_list = list(map(lambda x: x * 2, PRASHANT))
+print(do_list)
+pli = [1000, 2000, 3000, 4000, 5000]
+rli = [5.5, 55, 7, 1.5, 8, 4]
+tli = [2, 3, 4, 4, 6]
+si_li = list(map(lambda p, r, t: p * r * t / 100, pli, rli, tli))
+print(si_li)
+cs=[-22,1,-34,45,67,-98,78,11,-65,65]
+cs_obj=list(filter(lambda x:x>0,cs))
+print("Positive Numbers are: ",cs_obj)
+#Example of Reduced() Function
+#Sum of all elements
+from functools import reduce
+morning=[2,4,5,6,7,8,9,1,10]
+cs=reduce(lambda x,y : x + y, morning)
+print("Sum of elements in list is: ",cs)
