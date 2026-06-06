380c21f38bdb57a28acf4b319bfcac7b3ccc32de
Prashant Pandey <23prashantpandey@gmail.com>
2026-06-06 11:28:17 +0530

    Update WORKSHOP DAY 7.py

--- WORKSHOP DAY 7.py
+++ WORKSHOP DAY 7.py
@@ -28,3 +28,63 @@ except IndexError:
     print("Index is not correct")
 except ValueError:
     print("Provide correct value")
+try:
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+    if op == "+":
+        print("Result =", num1 + num2)
+    elif op == "-":
+        print("Result =", num1 - num2)
+    elif op == "*":
+        print("Result =", num1 * num2)
+    elif op == "/":
+        if num2 == 0:
+            print("Cannot divide by zero")
+        else:
+            print("Result =", num1 / num2)
+    else:
+        print("Invalid operator")
+except ValueError:
+    print("Please enter valid numbers")
+except Exception as e:
+    print("Error:", e)
+#raise exception: The raise statement is used to explicitly trigger exception 
+def check_age(age):
+    if age<18:
+        raise ValueError("Age must be 18")
+    print("Age is correct")
+try:
+    check_age(15)
+except ValueError as e:
+    print("Error:", e)
+try:
+    followers = int(input("Enter the number of followers: "))
+    posts = int(input("Posts uploaded this month: "))
+    reels = int(input("Reels uploaded this month: "))
+    if followers < 100:
+        raise ValueError("Followers must be at least 100")
+    print("Profile data accepted")
+except ValueError as e:
+    print("Error:", e)
+Assertions are used to verify assumptions in the code.
+try:
+    mark = -5
+    assert mark >= 0,"Marks cannot be negative"
+except AssertionError as e:
+    print("Error: ",e)
+try:
+    attendance = int(input("Enter attendance percentage: "))
+    if attendance < 75:
+        raise ValueError("Student is detained due to low attendance.")
+    print("Student is eligible to appear for exams.")
+except ValueError as e:
+    print("Error:", e)
+try:
+    username = input("Enter a unique usernme: ")
+    password = input("Enter a unique password: ")
+    assert username == "admin","Invalid Username"
+    assert password == "python123","Invalid Password"
+    print("Login Successfully")
+except AssertionError as e:
+    print("error",e)
