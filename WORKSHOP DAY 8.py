1c5c6f2d31ee4adf740162ee1bd6d4c55e09e062
Prashant Pandey <23prashantpandey@gmail.com>
2026-06-09 10:25:48 +0530

    Add files via upload

---
+++ WORKSHOP DAY 8.py
@@ -0,0 +1,10 @@
+#OOP Object Programming
+minimizes the code length
+easy dibugging
+obj=atm()
+obj.pingenerate
+L = [23,24,25,26,27]
+print(type())
+L.upper()
+verify the main attribute
+simple class with int method
