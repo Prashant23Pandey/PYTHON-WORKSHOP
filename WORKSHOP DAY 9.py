091848b14286337cc84c3a6eecd0cb3d186c17c5
Prashant Pandey <23prashantpandey@gmail.com>
2026-06-09 11:05:30 +0530

    Update WORKSHOP DAY 9.py

--- WORKSHOP DAY 9.py
+++ WORKSHOP DAY 9.py
@@ -16,4 +16,36 @@ class Atm:
         self.pin = ''
         self.balance = 0
         self.menu()
-        
+class Atm:
+    def __init__(self):
+        self.pin = ''
+        self.balance = 0
+        self.menu()
+    def menu(self):
+        user_input = input("""
+Hi, how can I help you?
+1. Press 1 to create pin
+2. Press 2 to change pin
+3. Press 3 to check balance
+4. Press 4 to withdraw
+5. Press 5 to exit
+""")
+        if user_input == '1':
+            self.create_pin()
+        elif user_input == '2':
+            print("Change pin feature not implemented yet")
+        elif user_input == '3':
+            print("Check balance feature not implemented yet")
+        elif user_input == '4':
+            print("Withdraw feature not implemented yet")
+        elif user_input == '5':
+            exit()
+    def create_pin(self):
+        user_pin = input("Enter your pin: ")
+        self.pin = user_pin
+        user_balance = int(input("Enter balance: "))
+        self.balance = user_balance
+        print("PIN created successfully")
+        self.menu()
+# Create ATM object
+atm = Atm()
